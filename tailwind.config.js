1cb712275bd93125e3817bd3197caef7d00a4ddd
Matheus de Sousa <mhenrique.sousa43@gmail.com>
2023-12-02 18:50:37 -0300

    fix: fix tailwind import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/*.{ts}"],
+  content: [
+    "./index.html",
+    "./src/**/*.html",
+    "./src/**/*.vue",
+    "./src/**/*.jsx",
+    "./src/**/*.tsx",
+  ],
   theme: {
     extend: {},
   },
